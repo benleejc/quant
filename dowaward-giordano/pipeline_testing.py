c0a2eef5dc78a22b91a3b911c3f9159a6612cf4f
ben-fedora <benleejc@gmail.com>
2024-03-26 18:16:37 +0800

    Add ATR pipeline

--- dowaward-giordano/pipeline_testing.py
+++ dowaward-giordano/pipeline_testing.py
@@ -1,35 +1,56 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: hydrogen
-#       format_version: '1.3'
-#       jupytext_version: 1.16.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# %%
 from data.data_pipelines import etl_dowaward_giordano
+import pandas as pd
+import numpy as np
 
-# %%
-%load_ext autoreload
-%autoreload 2
+def display_options():
+    display = pd.options.display
+    display.max_columns = 10
+    display.max_rows = None
+    display.max_colwidth = 222
+    display.width = None
+    return None
 
-# %%
-df = etl_dowaward_giordano()
-
-
-# %%
-def calculate_correlation(df):
-    # TODO aveage relative correlation calculation
-    pass
 
 def calculate_ATR(df):
-    pass
+    df.set_index('price_date', inplace=True)
+    df['prev_close'] = df.groupby('ticker')['close'].shift(1)
+    tr_components =  pd.concat([
+            (df['high'] - df['low']),
+            (df['high'] - df['prev_close']).abs(),
+            (df['low'] - df['prev_close']).abs()
+            ], axis=1)
+
+    df['tr'] = tr_components.max(axis=1)
+    # 42 period atr + highest close of 63 period
+    # 42 period atr - lowest low of 105 period
+    atr_period = 42
+    high_period = 63
+    low_period = 105
+    df['atr'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(atr_period).sum()) / 42
+    df['upper_band'] = df.groupby('ticker')['prev_close'].transform(lambda x: x.rolling(high_period).max())
+    df['lower_band'] = df.groupby('ticker')['low'].transform(lambda x: x.rolling(low_period).min())
+
+    #conds = [
+    #    (df['close'] >= df['atr_upper']),
+    #    (df['low'] < df['atr_lower'])
+    #]
+
+    #choices = [2,-2]
+    #df['atr'] = np.select(conds, choices)
+    print(df[df.ticker == 'SHY'][['close', 'high', 'upper_band', 'atr', 'tr']].iloc[0:100,:].to_csv('test.csv'))
+    #print(df[(df['close'] >= df['atr_upper'])])
+    #print(df[(df['low'] < df['atr_lower'])])
+            
+    return df 
 
 def rank_signals(df, columns_to_rank):
     pass
+
+def main():
+    df = etl_dowaward_giordano()
+    df = calculate_ATR(df)
+    return 
+
+if __name__ == '__main__':
+    display_options()
+    main()
